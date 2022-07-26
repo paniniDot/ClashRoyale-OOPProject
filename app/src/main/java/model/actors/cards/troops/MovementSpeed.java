db23b6526c436f736743acdb8d102563f90dcdab
Mattia Panni <mattia.panni@studio.unibo.it>
2022-07-26 15:34:02 +0200

    Add movementSpeed enum

---
+++ app/src/main/java/model/actors/cards/troops/MovementSpeed.java
@@ -0,0 +1,19 @@
+package model.actors.cards.troops;
+
+/**
+ * Speed at which entities move.
+ */
+public enum MovementSpeed {
+  /**
+   * Slow speed troops.
+   */
+  SLOW,
+  /**
+   * Medium speed troops.
+   */
+  MEDIUM,
+  /**
+   * Fast speed troops.
+   */
+  FAST 
+}
