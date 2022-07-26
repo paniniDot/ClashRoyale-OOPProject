e5a7e342ab7b5bb96e507bc230cd4a4fc2dd18d2
Mattia Panni <mattia.panni@studio.unibo.it>
2022-07-26 15:35:00 +0200

    Add UserLevel enum

---
+++ app/src/main/java/model/actors/users/UserLevel.java
@@ -0,0 +1,31 @@
+package model.actors.users;
+
+/**
+ * User level.
+ */
+public enum UserLevel {
+  /**
+   * Level 1.
+   */
+  LVL1,
+
+  /**
+   * Level 2.
+   */
+  LVL2,
+
+  /**
+   * Level 3.
+   */
+  LVL3,
+
+  /**
+   * Level 4.
+   */
+  LVL4,
+
+  /**
+   * Level 5.
+   */
+  LVL5;
+}
