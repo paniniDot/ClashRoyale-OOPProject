97d95eacb243fc3e71654e7f08849933766c1655
Mattia Panni <mattia.panni@studio.unibo.it>
2022-07-26 15:32:20 +0200

    Add javadoc

---
+++ app/src/main/java/model/actors/TargetType.java
@@ -0,0 +1,21 @@
+package model.actors;
+
+/**
+ * The self type of an Attackable entity.
+ */
+public enum TargetType {
+  /**
+   * Air troop.
+   */
+  AIR,
+
+  /**
+   * Ground troop.
+   */
+  GROUND,
+
+  /**
+   * Both air and ground troop.
+   */
+  BOTH,
+}
