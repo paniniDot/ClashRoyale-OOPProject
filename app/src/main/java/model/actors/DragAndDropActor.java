5b6644d0128707cdc4967a9badde5948344463dd
Mattia Panni <mattia.panni@studio.unibo.it>
2022-07-27 11:51:21 +0200

    Replace old DragAndDropActor implementation with a new one

--- app/src/main/java/model/actors/DragAndDropActor.java
+++ app/src/main/java/model/actors/DragAndDropActor.java
@@ -1,38 +1,105 @@
 package model.actors;
 
-
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
-import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Payload;
-import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop.Source;
+import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 
 /**
  * Actor who has DragAndDrop functionality.
  */
 public class DragAndDropActor extends BaseActor {
 
-  private final DragAndDrop dragAndDrop;
+  private final DragAndDropActor self;
+  private float grabOffsetX;
+  private float grabOffsetY;
+  private boolean isDraggable;
+
 
   /**
    * @param x
    *        x coordinate where the actor is placed.
    * @param y
    *        y coordinate where the actor is placed.
-   * @param stage
+   * @param s
    *        {@inheritDoc}
    */
-  public DragAndDropActor(final float x, final float y, final Stage stage) {
-    super(x, y, stage);
-    this.dragAndDrop = new DragAndDrop();
-    this.dragAndDrop.addSource(new Source(this) {
+  public DragAndDropActor(final float x, final float y, final Stage s) {
+    super(x, y, s);
+    self = this;
+    isDraggable = true;
+    this.addListener(new InputListener() {
 
       @Override
-      public Payload dragStart(final InputEvent event, final float x, final float y, final int pointer) {
-        Payload payload = new Payload();
-        payload.setDragActor(super.getActor());
-        return payload;
+      public boolean touchDown(final InputEvent event, final float eventOffsetX, final float eventOffsetY, final int pointer, final int button) {
+        if (!self.isDraggable()) {
+          return false;
+        }
+        self.grabOffsetX = eventOffsetX;
+        self.grabOffsetY = eventOffsetY;
+        self.toFront();
+        // increase size; object appears larger when lifted by player
+        self.addAction(Actions.scaleTo(1.1f, 1.1f, 0.25f));
+        self.onDragStart();
+        return true; // returning true indicates other touch methods are called
+      }
+
+      @Override
+      public void touchDragged(final InputEvent event, final float eventOffsetX, final float eventOffsetY, final int pointer) {
+        final float deltaX = eventOffsetX - self.grabOffsetX;
+        final float deltaY = eventOffsetY - self.grabOffsetY;
+        self.moveBy(deltaX, deltaY);
+      }
+
+      @Override
+      public void touchUp(final InputEvent event, final float eventOffsetX, final float eventOffsetY, final int pointer, final int button) {
+        // return object to original size when dropped by player
+        self.addAction(Actions.scaleTo(1.00f, 1.00f, 0.25f));
       }
     });
+
   }
+
+  /**
+   * Set whether this actor can be dragged.
+   * 
+   * @param d
+   *        true if actor can be dragged, false otherwise.
+   */
+  public void setDraggable(final boolean d) {
+    isDraggable = d;
+  }
+
+  /**
+   * Check if this actor can be dragged.
+   * 
+   * @return whether the actor is draggable or not.
+   */
+  public boolean isDraggable() {
+    return isDraggable;
+  }
+
+  /**
+   * Called when drag begins; extending classes may override this method.
+   */
+  public void onDragStart() {
+  }
+
+  /**
+   * Called when drop occurs; extending classes may override this method.
+   */
+  public void onDrop() {
+    super.setPosition(this.getUpdatedPosition().x, this.getUpdatedPosition().y);
+    System.out.println(super.getX() + ", " + super.getY());
+  }
+
+  /**
+   * 
+   * @return a vector2 containing the updated x,y positions of the actor.
+   */
+  public Vector2 getUpdatedPosition() {
+    return new Vector2(self.getX(), self.getY());
+  }
+
 }
