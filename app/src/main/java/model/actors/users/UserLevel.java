9653262c73a5320e30d602873187cb6b3e970086
Mattia Panni <mattia.panni@studio.unibo.it>
2022-07-29 13:15:04 +0200

    Add actualName field to userLevel enum

--- app/src/main/java/model/actors/users/UserLevel.java
+++ app/src/main/java/model/actors/users/UserLevel.java
@@ -7,25 +7,36 @@ public enum UserLevel {
   /**
    * Level 1.
    */
-  LVL1,
+  LVL1("Level 1"),
 
   /**
    * Level 2.
    */
-  LVL2,
+  LVL2("Level 2"),
 
   /**
    * Level 3.
    */
-  LVL3,
+  LVL3("Level 3"),
 
   /**
    * Level 4.
    */
-  LVL4,
+  LVL4("Level 4"),
 
   /**
    * Level 5.
    */
-  LVL5;
+  LVL5("Level 5");
+
+  private final String name;
+
+  UserLevel(final String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString() {
+    return this.name;
+  }
 }
