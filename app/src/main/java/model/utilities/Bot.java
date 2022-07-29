2dd930c2f88d4aae2d2159f41b77d3a502b0f02b
Mattia Panni <mattia.panni@studio.unibo.it>
2022-07-29 21:43:47 +0200

    Update import package

--- app/src/main/java/model/utilities/Bot.java
+++ app/src/main/java/model/utilities/Bot.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 import com.badlogic.gdx.math.Vector2;
 import model.actors.cards.Card;
-import model.utilities.inGameUtilities.GameMap;
+import model.utilities.ingame.GameMap;
 
 /**
  * bot implementation.
--- app/src/main/java/model/utilities/ingame/GameMap.java
+++ app/src/main/java/model/utilities/ingame/GameMap.java
@@ -1,4 +1,4 @@
-package model.utilities.inGameUtilities;
+package model.utilities.ingame;
 
 import java.util.List;
 import java.util.stream.Collectors;
