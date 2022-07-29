5bc030b6fd89cdf6434511861227782ab9a63eed
Mattia Panni <mattia.panni@studio.unibo.it>
2022-07-29 18:52:45 +0200

    Move Speeds enum to a new package

---
+++ app/src/main/java/model/actors/Speeds.java
@@ -0,0 +1,19 @@
+package model.actors;
+
+/**
+ * Speed at which entities move.
+ */
+public enum Speeds {
+  /**
+   * Slow speed troops.
+   */
+  SLOW,
+  /**
+   * Medium speed troops.
+   */
+  MEDIUM,
+  /**
+   * Fast speed troops.
+   */
+  FAST 
+}
