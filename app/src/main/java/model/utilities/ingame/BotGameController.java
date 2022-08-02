7c681919adae7cbd267c6c9cf1d41f8aa7e45527
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2022-08-02 16:00:01 +0200

    fix kingTower position

--- app/src/main/java/model/utilities/ingame/BotGameController.java
+++ app/src/main/java/model/utilities/ingame/BotGameController.java
@@ -54,7 +54,7 @@ public class BotGameController extends GameController {
     final List<Tower> towers = new ArrayList<>();
     towers.add(QueenTower.create(bot, stage, new Vector2(205, 613)));
     towers.add(QueenTower.create(bot, stage, new Vector2(415, 613)));
-    towers.add(KingTower.create(bot, stage, new Vector2(300, 658)));
+    towers.add(KingTower.create(bot, stage, new Vector2(300, 640)));
     towers.forEach(t -> {
       if (t.getClass() == QueenTower.class) {
         t.setAnimation(AnimationUtilities.loadTexture("towers/enemy/queen_tower.png"));
