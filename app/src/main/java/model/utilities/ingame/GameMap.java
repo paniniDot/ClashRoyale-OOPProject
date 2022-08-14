0468565d8c45b82849ab3b5568e328898a9f195a
Mattia Panni <mattia.panni@studio.unibo.it>
2022-08-14 16:48:15 +0200

    Load towers and obstacles from json files instead of storing them in the code

--- app/src/main/java/model/utilities/ingame/GameMap.java
+++ app/src/main/java/model/utilities/ingame/GameMap.java
@@ -1,14 +1,21 @@
 package model.utilities.ingame;
 
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import org.jgrapht.Graph;
 import org.jgrapht.alg.shortestpath.AStarShortestPath;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.builder.GraphTypeBuilder;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.math.Vector2;
+import com.google.common.reflect.TypeToken;
+import com.google.gson.Gson;
 
 import model.actors.Attackable;
 import model.utilities.VectorsUtilities;
@@ -23,7 +30,8 @@ public class GameMap {
   private static final int Y_START = 255;
 
   private final Graph<MapUnit, DefaultEdge> map;
-
+  private final List<Vector2> obstaclePositions;
+  private final List<Vector2> towerPositions;
   /**
    * Constructor.
    */
@@ -35,21 +43,21 @@ public class GameMap {
         .weighted(false)
         .edgeClass(DefaultEdge.class)
         .buildGraph();
-
+    this.towerPositions = this.getTowers();
+    this.obstaclePositions = this.getObstacles();
     this.addVerteces();
     this.addEdges();
   }
 
   private void addVerteces() {
     int x = X_START;
-    int y = Y_START;
+    int y;
     for (int i = 1; i < HORIZONTAL_UNITS; i++) {
       y = Y_START;
       for (int j = 1; j < VERTICAL_UNITS; j++) {
         final var coords = new Vector2(i, j);
-        if (!this.getObstacles().contains(coords)) {
-          final var vertex = new MapUnit(coords, new Vector2(x, y), this.getTowers().contains(coords) ? MapUnit.Type.TOWER : MapUnit.Type.TERRAIN);
-          // System.out.println(vertex);
+        if (!this.obstaclePositions.contains(coords)) {
+          final var vertex = new MapUnit(coords, new Vector2(x, y), this.towerPositions.contains(coords) ? MapUnit.Type.TOWER : MapUnit.Type.TERRAIN);
           this.map.addVertex(vertex);
         }
         y += MapUnit.HEIGHT;
@@ -59,186 +67,37 @@ public class GameMap {
   }
 
   private void addEdges() {
-    for (final var vertex : this.map.vertexSet()) {
-      if (vertex.getCoordinates().x > 1 && vertex.getCoordinates().y > 1) {
-      for (var i = vertex.getCoordinates().x - 1; i <= vertex.getCoordinates().x + 1; i++) {
-        for (var j = vertex.getCoordinates().y - 1; j <= vertex.getCoordinates().y + 1; j++) {
-          if (!this.getObstacles().contains(new Vector2(i, j))) {
-          final var mapUnit = new MapUnit(new Vector2(i, j), new Vector2(X_START + MapUnit.WIDTH * (i - 1), Y_START + MapUnit.HEIGHT * (j - 1)), this.getTowers().contains(new Vector2(i, j)) ? MapUnit.Type.TOWER : MapUnit.Type.TERRAIN);
-          if (this.map.containsVertex(mapUnit) && !vertex.equals(mapUnit)) {
-            this.map.addEdge(vertex, mapUnit);
-          }
-          }
-        }
-      }
-      }
+    this.map.vertexSet().stream()
+    .filter(src -> this.checkForOutOfBounds(src.getCoordinates()))
+    .forEach(src -> Stream.iterate(src.getCoordinates().x - 1, n -> n <= src.getCoordinates().x + 1, n -> n + 1)
+        .forEach(i -> Stream.iterate(src.getCoordinates().y - 1, n -> n <= src.getCoordinates().y + 1, n -> n + 1)
+            .forEach(j -> Stream.of(new Vector2(i, j))
+                .filter(v -> !this.obstaclePositions.contains(v))
+                .map(v -> new MapUnit(v, this.getPixelsFromUnitCoords(v), this.towerPositions.contains(v) ? MapUnit.Type.TOWER : MapUnit.Type.TERRAIN))
+                .filter(dst -> this.map.containsVertex(dst) && !src.equals(dst))
+                .forEach(dst -> this.map.addEdge(src, dst)))));
+  }
+
+  private boolean checkForOutOfBounds(final Vector2 pos) {
+    return pos.x > 1 && pos.y > 1;
+  }
+
+  @SuppressWarnings("serial")
+  private List<Vector2> getTowers() {
+    try {
+      return new Gson().fromJson(new FileReader(Gdx.files.internal("saves/Arena1Resources/towers.json").file()), new TypeToken<List<Vector2>>() { }.getType());
+    } catch (IOException e) {
+      return Collections.emptyList();
     }
   }
 
-  public List<Vector2> getTowers() {
-    return List.of(
-        new Vector2(4,6),
-        new Vector2(4,7),
-        new Vector2(4,8),
-        new Vector2(4,9),
-        new Vector2(9,1),
-        new Vector2(9,2),
-        new Vector2(9,3),
-        new Vector2(9,4),
-        new Vector2(9,5),
-        new Vector2(9,6),
-
-        new Vector2(10,1),
-        new Vector2(10,2),
-        new Vector2(10,3),
-        new Vector2(10,4),
-        new Vector2(10,5),
-        new Vector2(10,6),
-
-        new Vector2(15,6),
-        new Vector2(15,7),
-        new Vector2(15,8),
-        new Vector2(15,9),
-
-        new Vector2(4,25),
-        new Vector2(4,26),
-        new Vector2(4,27),
-
-        new Vector2(9,28),
-        new Vector2(9,29),
-        new Vector2(9,30),
-        new Vector2(9,31),
-        new Vector2(10,28),
-        new Vector2(10,29),
-        new Vector2(10,30),
-        new Vector2(10,31),
-
-        new Vector2(15,25),
-        new Vector2(15,26),
-        new Vector2(15,27)
-
-
-        );
-  }
-
-  public List<Vector2> getObstacles() {
-    return List.of(
-        new Vector2(3,5),
-        new Vector2(3,6),
-        new Vector2(3,7),
-        new Vector2(3,8),
-        new Vector2(3,9),
-        new Vector2(4,5),
-        new Vector2(5,5),
-        new Vector2(5,6),
-        new Vector2(5,7),
-        new Vector2(5,8),
-        new Vector2(5,9),
-
-        new Vector2(8,1),
-        new Vector2(8,2),
-        new Vector2(8,3),
-        new Vector2(8,4),
-        new Vector2(8,5),
-        new Vector2(8,6),
-
-        new Vector2(11,1),
-        new Vector2(11,2),
-        new Vector2(11,3),
-        new Vector2(11,4),
-        new Vector2(11,5),
-        new Vector2(11,6),
-
-        new Vector2(3,25),
-        new Vector2(3,26),
-        new Vector2(3,27),
-        new Vector2(3,28),
-        new Vector2(4,28),
-        new Vector2(5,25),
-        new Vector2(5,26), 
-        new Vector2(5,27),
-        new Vector2(5,28),
-        new Vector2(8,28),
-        new Vector2(8,29),
-        new Vector2(8,30),
-        new Vector2(8,31),
-        new Vector2(11,28),
-        new Vector2(11,29),
-        new Vector2(11,30),
-        new Vector2(11,31),
-        new Vector2(14,25),
-        new Vector2(14,26),
-        new Vector2(14,27),
-        new Vector2(14,28),
-        new Vector2(15,28),
-        new Vector2(16,25),
-        new Vector2(16,26), 
-        new Vector2(16,27),
-        new Vector2(16,28),
-
-        new Vector2(14,5),
-        new Vector2(14,6),
-        new Vector2(14,7),
-        new Vector2(14,8),
-        new Vector2(14,9),
-        new Vector2(15,5),
-        new Vector2(16,5),
-        new Vector2(16,6),
-        new Vector2(16,7),
-        new Vector2(16,8),
-        new Vector2(16,9),
-
-        new Vector2(1,15), 
-        new Vector2(2,15), 
-        new Vector2(3,15), 
-        new Vector2(1,16), 
-        new Vector2(2,16), 
-        new Vector2(3,16),
-        new Vector2(1,17), 
-        new Vector2(2,17), 
-        new Vector2(3,17),
-
-        new Vector2(5,15), 
-        new Vector2(6,15), 
-        new Vector2(7,15),
-        new Vector2(8,15), 
-        new Vector2(9,15), 
-        new Vector2(10,15),
-        new Vector2(11,15), 
-        new Vector2(12,15), 
-        new Vector2(13,15),
-        new Vector2(14,15), 
-        new Vector2(5,16), 
-        new Vector2(6,16), 
-        new Vector2(7,16),
-        new Vector2(8,16), 
-        new Vector2(9,16), 
-        new Vector2(10,16),
-        new Vector2(11,16), 
-        new Vector2(12,16), 
-        new Vector2(13,16),
-        new Vector2(14,16),
-        new Vector2(5,17), 
-        new Vector2(6,17), 
-        new Vector2(7,17),
-        new Vector2(8,17), 
-        new Vector2(9,17), 
-        new Vector2(10,17),
-        new Vector2(11,17), 
-        new Vector2(12,17), 
-        new Vector2(13,17),
-        new Vector2(14,17),
-
-        new Vector2(16,15), 
-        new Vector2(17,15), 
-        new Vector2(18,15),
-        new Vector2(16,16), 
-        new Vector2(17,16), 
-        new Vector2(18,16),
-        new Vector2(16,17), 
-        new Vector2(17,17), 
-        new Vector2(18,17)
-        );
+  @SuppressWarnings("serial")
+  private List<Vector2> getObstacles() {
+    try {
+      return new Gson().fromJson(new FileReader(Gdx.files.internal("saves/Arena1Resources/obstacles.json").file()), new TypeToken<List<Vector2>>() { }.getType());
+    } catch (IOException e) {
+      return Collections.emptyList();
+    }
   }
 
   /**
@@ -255,7 +114,7 @@ public class GameMap {
           .getPath(source, dest)
           .getVertexList()
           .stream()
-          .filter(el -> !this.getTowers().contains(el.getCoordinates()))
+          .filter(el -> !this.towerPositions.contains(el.getCoordinates()))
           .map(MapUnit::getPosition)
           .collect(Collectors.toList());
     }
@@ -297,7 +156,7 @@ public class GameMap {
 
   private MapUnit getMapUnitFromPosition(final Vector2 pixels) {
     final var coords = new Vector2((float) Math.ceil((pixels.x - X_START + 1) / MapUnit.WIDTH), (float) Math.ceil((pixels.y - Y_START + 1) / MapUnit.HEIGHT));
-    return new MapUnit(coords, this.getPixelsFromUnitCoords(coords), this.getTowers().contains(coords) ? MapUnit.Type.TOWER : MapUnit.Type.TERRAIN);
+    return new MapUnit(coords, this.getPixelsFromUnitCoords(coords), this.towerPositions.contains(coords) ? MapUnit.Type.TOWER : MapUnit.Type.TERRAIN);
   }
 
   private Vector2 getPixelsFromUnitCoords(final Vector2 coords) {
