ca5118b5e65f45054e5bd0a9bce13170d63cf3c8
Riccardo Fiorani <riccardo.fiorani2@studio.unibo.it>
2022-08-25 13:53:23 +0200

    fix updateActorPositions in BotGameController and restore GlobalData from commit 5c175d1dcc323a7be8af6bdaf24e1baeadc31317

--- app/src/main/java/model/GlobalData.java
+++ app/src/main/java/model/GlobalData.java
@@ -34,62 +34,26 @@ public final class GlobalData {
   /**
    * Provides the User deck.
    */
-  public static final List<Card> DEFAULT_USER_DECK = List.of(
+  public static final List<Card> USER_DECK = List.of(
       Wizard.create(USER, new Vector2(100, 100)), 
-      Wizard.create(USER, new Vector2(200, 100)), 
-      Giant.create(USER, new Vector2(300, 100)), 
-      Wizard.create(USER, new Vector2(400, 100)), 
-      Barbarian.create(USER, new Vector2(500, 100)),
-      FireBall.create(USER, new Vector2(600, 100)),
-      Wizard.create(USER, new Vector2(700, 100)));
+      Giant.create(USER, new Vector2(200, 100)), 
+      Barbarian.create(USER, new Vector2(300, 100)), 
+      FireBall.create(USER, new Vector2(400, 100)), 
+      Wizard.create(USER, new Vector2(500, 100)),
+      Wizard.create(USER, new Vector2(600, 100)));
 
   /**
    * Provides the Bot deck.
    */
   public static final List<Card> BOT_DECK = List.of(
-      Giant.create(BOT, new Vector2(100, 800)), 
-      Barbarian.create(BOT, new Vector2(200, 800)), 
+      Wizard.create(BOT, new Vector2(100, 800)), 
+      Giant.create(BOT, new Vector2(200, 800)), 
       Barbarian.create(BOT, new Vector2(300, 800)), 
-      Barbarian.create(BOT, new Vector2(400, 800)), 
-      Barbarian.create(BOT, new Vector2(500, 800)),
-      Barbarian.create(BOT, new Vector2(600, 800)),
-      Barbarian.create(BOT, new Vector2(700, 800)));
+      Wizard.create(BOT, new Vector2(400, 800)), 
+      Wizard.create(BOT, new Vector2(500, 800)),
+      Wizard.create(BOT, new Vector2(600, 800)));
 
-  private final List<Card> userDeck;
-
-  /**
-   * Constructor.
-   */
-  public GlobalData() {
-    this.userDeck = new ArrayList<>();
-
-    /*final List<Vector2> position = List.of(
-        new Vector2(100, 100),
-        new Vector2(200, 100),
-        new Vector2(300, 100),
-        new Vector2(400, 100),
-        new Vector2(500, 100),
-        new Vector2(600, 100),
-        new Vector2(700, 100));
-    var i=0;
-    */
-
-    //Caricare il file json
-
-    //Switch per ogni elemento caricato e creare l'apposita truppa
-    //es: case "Barbarian": this.userDeck.add(Barbarian.create(User, position.get(i));
-    //      i++; break;
-  }
-
-  /**
-   * @return the User deck.
-   */
-  public List<Card> getUserDeck() {
-    if (this.userDeck.isEmpty()) {
-      return GlobalData.DEFAULT_USER_DECK;
-    } else {
-      return this.userDeck;
-    }
+  private GlobalData() {
   }
 
 
