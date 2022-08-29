4629c7b396934df6bb284a18d80040cb1629b9fd
riccardo fiorani <64063131+fiorani@users.noreply.github.com>
2022-08-29 22:03:48 +0200

    refactor checkposition

--- app/src/main/java/controller/game/BotGameController.java
+++ app/src/main/java/controller/game/BotGameController.java
@@ -160,15 +160,9 @@ public class BotGameController extends GameController {
   }
 
   private boolean checkposition(final Vector2 v, final Card c) {
-    if (super.getGameMap().containsPosition(v) && super.getGameMap().getMapUnitFromPosition(v).getType().equals(MapUnit.Type.TERRAIN)) {
-      if (c.getOwner() instanceof User && v.y < SIDE) {
-        return true;
-      } else if (c.getOwner() instanceof Bot && v.y > SIDE) {
-        return true;
-      }
-    }
-    return false;
+    return super.getGameMap().containsPosition(v) && super.getGameMap().getMapUnitFromPosition(v).getType().equals(MapUnit.Type.TERRAIN) && c.getOwner() instanceof User && v.y < SIDE || v.y > SIDE;
   }
+
   private void deployBotCard(final Card card) {
     ((BotGameModel) super.getModel()).deployBotCard(card);
     this.botElixir.decrementElixir(card.getCost());
