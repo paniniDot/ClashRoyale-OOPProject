92b50813811a9a71da307643e82d4f4b4e102745
Alexandro Salvato <salvato.alexandro@gmail.com>
2022-08-29 15:51:53 +0200

    Updated FileManager

--- app/src/main/java/controller/FileManager.java
+++ app/src/main/java/controller/FileManager.java
@@ -7,15 +7,16 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Optional;
 
 /**
  * Used to save/read stats into/from a file.
  */
 public class FileManager {
 
-  private int plays;
-  private int wins;
-  private int towersDestroyed;
+  private Optional<Integer> plays;
+  private Optional<Integer> wins;
+  private Optional<Integer> towersDestroyed;
 
   private static final String ABSOLUTEPATH = "saves" + File.separator + "stats.txt";
 
@@ -30,42 +31,60 @@ public class FileManager {
    * @return the number of game played.
    */
   public int getPlays() {
-    return this.plays;
+    if (this.plays.isPresent()) { 
+      return this.plays.get();
+    } else {
+    return 0;
+    }
   }
 
   /**
    * Increment the games played counter.
    */
   public void addPlays() {
-    this.plays++;
+    var t = getPlays();
+    t++;
+    this.plays = Optional.of(t);
   }
 
   /**
    * @return the number of game won.
    */
   public int getWins() {
-    return this.wins;
+    if (this.wins.isPresent()) { 
+      return this.wins.get();
+    } else {
+    return 0;
+    }
   }
 
   /**
    * Increment the games played counter.
    */
   public void addWin() {
-    this.wins++;
+    var t = getWins();
+    t++;
+    this.wins = Optional.of(t);
   }
 
   /**
    * @return the number of towers destroyed.
    */
   public int getTowersDestroyed() {
-    return this.towersDestroyed;
+    if (this.towersDestroyed.isPresent()) { 
+      return this.towersDestroyed.get();
+    } else {
+    return 0;
+    }
   }
 
   /**
    * @param towersDestroyed the number of towers destroyed.
    */
   public void addTowersDestroyed(final int towersDestroyed) {
-    this.towersDestroyed += towersDestroyed;
+    var t = getTowersDestroyed();
+    t += towersDestroyed;
+    this.towersDestroyed = Optional.of(t);
   }
 
   /**
@@ -89,15 +108,15 @@ public class FileManager {
   /**
    * Read the data from file and updates the field.
    */
-  public void read() {
+  public final void read() {
     try (BufferedReader bufferedReader = new BufferedReader(new FileReader(FileManager.ABSOLUTEPATH))) {
 
       var temp = bufferedReader.read();
-      this.plays = temp;
+      this.plays = Optional.of(temp);
       temp = bufferedReader.read();
-      this.wins = temp;
+      this.wins = Optional.of(temp);
       temp = bufferedReader.read();
-      this.towersDestroyed = temp;
+      this.towersDestroyed = Optional.of(temp);
 
     } catch (FileNotFoundException e) {
       this.initialize();
