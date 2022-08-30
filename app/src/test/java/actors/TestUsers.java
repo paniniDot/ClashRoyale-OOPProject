e63257d5f66bcacfa87c9c3445962e8653c8520c
Mattia Panni <mattia.panni@studio.unibo.it>
2022-08-30 21:01:13 +0200

    Remove PMD, checkstyle warnings from user test class

--- app/src/test/java/actors/TestUsers.java
+++ app/src/test/java/actors/TestUsers.java
@@ -1,6 +1,6 @@
 package actors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -13,12 +13,12 @@ class TestUsers {
   private User panini;
 
   @BeforeEach
-  public void setUp() {
+  void setUp() {
     this.panini = new User("panini");
   }
 
   @Test
-  public void simpleTest() {
+  void simpleTest() {
     assertEquals(this.panini.getName(), "panini");
     assertEquals(this.panini.getCurrentXP(), 0);
     assertEquals(this.panini.getCurrentLevel(), UserLevel.LVL1);
@@ -26,23 +26,27 @@ class TestUsers {
 
   @Test
   void testLevelUp() {
-    this.panini.awardXp(50);
+    final int firstAward = 50;
+    final int secondAward = 300;
+    this.panini.awardXp(firstAward);
     assertEquals(this.panini.getCurrentLevel(), UserLevel.LVL1);
-    this.panini.awardXp(50);
+    this.panini.awardXp(firstAward);
     assertEquals(this.panini.getCurrentLevel(), UserLevel.LVL2);
-    this.panini.awardXp(300);
+    this.panini.awardXp(secondAward);
     assertEquals(this.panini.getCurrentLevel(), UserLevel.LVL3);
   }
 
   @Test
   void testMultipleLevelsUp() {
-    this.panini.awardXp(400);
+    final int award = 400;
+    this.panini.awardXp(award);
     assertEquals(this.panini.getCurrentLevel(), UserLevel.LVL3);
   }
 
   @Test
   void testOutOfBoundLevelUp() {
-    this.panini.awardXp(3000);
+    final int award = 3000;
+    this.panini.awardXp(award);
     assertEquals(this.panini.getCurrentLevel(), UserLevel.LVL5);
   }
 }
